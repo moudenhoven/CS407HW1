c836e9b5d07824b79dcd330da359459738a8ab4e
oudie33 <MichaelOudenhoven@michaels-macbook-pro.local>
2016-02-09 09:07:00 -0600

    Initial Commit

---
+++ CS407HW1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS407HW1
+//
+//  Created by Michael Oudenhoven on 2/9/16.
+//  Copyright © 2016 Michael Oudenhoven. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
